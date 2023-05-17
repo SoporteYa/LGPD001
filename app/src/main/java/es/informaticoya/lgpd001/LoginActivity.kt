d4a78c031143b1e1e6f6c8587b75e3812591bef8
SoporteYa <133646077+SoporteYa@users.noreply.github.com>
2023-05-17 15:12:41 +0200

    Add files via upload

--- app/src/main/java/es/informaticoya/lgpd001/LoginActivity.kt
+++ app/src/main/java/es/informaticoya/lgpd001/LoginActivity.kt
@@ -3,22 +3,44 @@ package es.informaticoya.lgpd001
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import es.informaticoya.lgpd001.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+
         binding.btnSignUp.setOnClickListener {
-            startActivity(Intent(this, SignUpActivity::class.java))
+            if(binding.etEmail.text.isEmpty() && binding.etPassword.text.isEmpty()){
+                Toast.makeText(this, "Rellenar datos", Toast.LENGTH_SHORT).show()
+            }else{
+                authentication()
+            }
         }
-
-
     }
 
-
+    private fun authentication(){
+        auth.createUserWithEmailAndPassword(binding.etEmail.text.toString(), binding.etPassword.text.toString())
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful){
+                    finish()
+                    startActivity(Intent(this, SignUpActivity::class.java))
+                    Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Registro no realizado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
