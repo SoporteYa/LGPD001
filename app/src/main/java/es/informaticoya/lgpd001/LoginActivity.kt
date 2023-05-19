ece3fea8f144420981c419b13538d10c45331980
SoporteYa <133646077+SoporteYa@users.noreply.github.com>
2023-05-19 09:15:29 +0200

    Add files via upload

--- app/src/main/java/es/informaticoya/lgpd001/LoginActivity.kt
+++ app/src/main/java/es/informaticoya/lgpd001/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import es.informaticoya.lgpd001.databinding.ActivityLoginBinding
@@ -15,6 +16,9 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -22,25 +26,41 @@ class LoginActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
-        binding.btnSignUp.setOnClickListener {
-            if(binding.etEmail.text.isEmpty() && binding.etPassword.text.isEmpty()){
-                Toast.makeText(this, "Rellenar datos", Toast.LENGTH_SHORT).show()
-            }else{
-                authentication()
+
+        binding.btnLogin.setOnClickListener {
+
+            binding.btnLogin.setOnClickListener {
+
+                if (binding.etEmail.text.isEmpty() && binding.etPassword.text.isEmpty()) {
+                    Toast.makeText(this, "Rellenar datos", Toast.LENGTH_SHORT).show()
+                } else {
+                    authentication()
+                }
             }
         }
     }
 
-    private fun authentication(){
-        auth.createUserWithEmailAndPassword(binding.etEmail.text.toString(), binding.etPassword.text.toString())
+    private fun authentication() {
+        auth.createUserWithEmailAndPassword(
+            binding.etEmail.text.toString(),
+            binding.etPassword.text.toString())
             .addOnCompleteListener(this) {
-                if (it.isSuccessful){
+                if (it.isSuccessful) {
                     finish()
                     startActivity(Intent(this, SignUpActivity::class.java))
                     Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this, "Registro no realizado", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Registro no realizado", Toast.LENGTH_SHORT).show()
+                }
             }
         }
+
+    override fun onStart() {
+        super.onStart()
+        val user = auth.currentUser
+        if (user != null) {
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }
+      }
     }
-}
